ce5f8b407a2a83f382612d831a0c0984fa73f73c
Brody Currie <brodycurrie@gmail.com>
2019-05-27 14:04:43 -0400

    Completed reinforcement exercise

--- day1_reinforcement.py
+++ day1_reinforcement.py
@@ -1,3 +1,15 @@
 digits = ['1','2','3','4','5','6','7','8','9']
 en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+jap = ['ichi', 'ni', 'san', 'shi', 'go', 'roku', 'shichi', 'hachi', 'ku']
+
+numbers = {}
+
+for num in range(0, len(digits)):
+  numbers[digits[num]] = {
+    'french': fr[num],
+    'english': en[num],
+    'japanese': jap[num]
+  }
+
+print(numbers)
