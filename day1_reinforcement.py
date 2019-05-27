6800ebe18a0e747c6eb6a96c13ccc33636955c8c
Brody Currie <brodycurrie@gmail.com>
2019-05-27 12:16:20 -0400

    initial commit

---
+++ day1_reinforcement.py
@@ -0,0 +1,3 @@
+digits = ['1','2','3','4','5','6','7','8','9']
+en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
