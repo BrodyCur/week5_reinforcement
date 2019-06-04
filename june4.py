afe1a1931025ce59e923ff795cf9a5d24b4162ab
Brody Currie <brodycurrie@gmail.com>
2019-06-04 10:47:19 -0400

    completed exercise

---
+++ june4.py
@@ -0,0 +1,13 @@
+conference = [
+{'id': "38fj8d900", 'city': 'Hamilton', 'events': [{'date': '2017-01-01', 'attendees': 100}, {'date': '2016-12-31', 'attendees': 60}]},
+{'id': "39fo837y7", 'city': 'Toronto', 'events': [{'date': '2017-03-30', 'attendees': 3000}, {'date': '2017-07-07', 'attendees': 2500}, {'date': '2017-02-04', 'attendees': 900}]},
+{'id': "58uj8d800", 'city': 'Montreal', 'events': [{'date': '2017-08-10', 'attendees': 250}]},
+{'id': "48hn8d900", 'city': 'Kingston', 'events': [{ 'date': '2015-04-16', 'attendees': 45}]}
+]
+
+for event in conference:
+  print(event['city'])
+  print('---------------')
+  for event_info in event['events']:
+    print('Date: ' + event_info['date'] + ',' + str(event_info['attendees']) + ' People')
+  print('\n')
